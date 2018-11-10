34de032e3609d7b31167e2a6ed7c2d0ee78fe24a
stersylva <stersylva@gmail.com>
2018-11-10 12:23:05 -0300

    'operadores aritimetricos e operadores relacionais'

---
+++ exercicios-js/fundamentos/aritimetricos.js
@@ -0,0 +1,10 @@
+// Operadores binários precisam de 2 operando
+const [a, b, c, d] = [3, 5, 1, 15]
+
+const soma = a + b + c + d
+const subtracao = d - b
+const multiplicacao = a * b
+const divisao = d / a
+const modulo =  a % 2
+
+console.log(soma, subtracao, multiplicacao, divisao, modulo)
