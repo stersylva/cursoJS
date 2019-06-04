1d5da59a3b362d325ce9cd12792c9f5862fb7d5f
stersylva <stersylva@gmail.com>
2019-06-03 22:37:03 -0300

    concat, fatMapp, Imperativo X Declarativo

---
+++ exercicios-js/array/concat.js
@@ -0,0 +1,7 @@
+const filhas = ['maria', 'paula']
+const filhos = ['pedro', 'jose']
+const todos = filhas.concat(filhos)
+
+console.log(todos, filhas, filhos)
+
+console.log([].concat([1 ,2], [3, 4], 5, [[6, 7]]))
